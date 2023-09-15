7b901b957119316a9d0ee620c0d50036b10fe512
yash433 <parekhyash9999@gmail.com>
2023-09-14 20:24:39 -0400

    Add files via upload

---
+++ NLP Final Project/finalScript.py
@@ -0,0 +1,34 @@
+from textblob import TextBlob
+
+def file_content(filename):
+    t = open(filename)
+    tt = t.read()
+    return tt
+
+#connect textblob, text, and sentences above
+blob = TextBlob(file_content("demofile.txt"))
+sentences = blob.sentences
+
+#This is suppose to be my translation feature, but it does not work because Google stopped the service
+'''
+if (blob.detect_language() != 'en'):
+    print(blob.translate(to='en'))
+'''
+
+for x in sentences:
+    print(x)
+
+#gives polairty number for each sentence
+for sentence in blob.sentences:
+    print('Sentence Polarity')
+    print(sentence.sentiment.polarity)
+
+    
+#gives sentiment (polairty & subjectivity) to the entire review
+print(blob.sentiment)
+
+#identifies and prints ONLY noun phrases that help restaurants search for key words in reviews easily
+print('Noun Phrases:')
+print(blob.noun_phrases)
+
+
